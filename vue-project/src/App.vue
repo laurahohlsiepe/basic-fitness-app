1ed51a9223b23d298fc466bc03d2578dfca69348
laurahohlsiepe <l.hohlsiepe@gmail.com>
2023-06-24 20:25:20 +0200

    first commit: basic structure and functions

---
+++ vue-project/src/App.vue
@@ -0,0 +1,89 @@
+
+
+
+<template>
+  <header>
+      <div class="flex flex-wrap justify-center items-center mx-auto max-w-screen-xl">
+         <h1 class="text-pink-400 text-2xl text-center mb-4">This the most basic Fitness App ever</h1>       
+      </div>
+  </header>
+  <body class=" full-screen">
+
+  <section class="flex flex-col md:flex-col sm:mx-16 lg:mx-20">
+    <h2 class="text-2xl text-center py-6">Legday</h2>
+  <div class="flex flex-col justify-center align-between">
+
+    <div class="flex justify-center">
+         <input type="text" class="w-1/2 rounded bg-fuchsia-200 px-5 py-2.5 mr-2 mt-2" v-model="workoutText" placeholder="add new workout here with sets and repetitions"> 
+    </div>
+
+    <div class="flex justify-center m-6">
+          <label for="reps" class="mx-4">Choose your reps</label>
+    <select v-model="selectedValue">
+      <option v-for="repetition in repetitions" :key="repetition.value" :value="repetition.value">{{ repetition.name }}</option>
+    </select>
+    </div>
+
+    <div class="flex justify-center">
+      <button class="text-white bg-pink-400 font-medium rounded-lg px-5 py-2.5 w-32 h-1/3 text-center" type="submit" @click="addWorkout()">send</button>    
+    </div>
+
+  </div>
+
+    <div class="overflow-x-auto mt-6">
+       <table class="table-fixed w-full text-sm text-left text-gray-500 dark:text-gray-400">
+      <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+        <tr>
+          <th scope="col" class="px-6 py-3">Name</th>
+          <th scope="col" class="px-6 py-3">Reps</th>
+      </tr>
+      </thead>
+      <tbody>
+        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+          <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">RDLs</th>
+          <td class="px-6 py-4">reps</td>
+        </tr>
+        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+          <th  scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">Squats</th>
+          <td class="px-6 py-4">reps</td>
+        </tr>
+        <tr v-for="workout in workouts" class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+          <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">{{ workout.text }}</td>
+          <td class="px-6 py-4">{{ selectedValue }}</td>
+        </tr>
+      </tbody>
+    </table>   
+    </div>
+  </section>
+</body>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      workouts: [],
+      workoutoutText: '',
+      selectedValue: '',
+      repetitions: [
+     {value: 'easy', name: 'Easy'},
+     {value: 'medium', name: 'Medium'},
+     {value: 'advanced', name: 'Advanced'}
+      ]
+    }
+  },
+  methods: {
+    addWorkout() {
+      const newWorkouts = this.workoutText;
+      this.workoutoutText = '';
+      this.workouts = [
+        ...this.workouts,
+        {text: newWorkouts}
+      ]
+    }
+  }
+}
+</script>
+
+
